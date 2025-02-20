7dab3702f6ac6af86542d46f7936d9fcb2ddd469
PASHOX <Tok@mail.ru>
2025-02-20 13:01:56 +0300

    Первый урок по js

--- src/js/script.js
+++ src/js/script.js
@@ -1,1 +1,18 @@
-alert ('fif');
+"use strict";
+
+// prompt('Сколько фильмов вы посмотрели?', '');
+
+const numberOfFilms = prompt('Сколько фильмов вы посмотрели?', '');
+
+const personaMovieDB = {
+  count: numberOfFilms,
+  movies: {},
+  actors: {},
+  genres: [],
+  private: false
+};
+const a = prompt("Один из последних просмотренных фильмов", ""),
+  b = prompt("На сколько оцените его", "");
+ 
+personaMovieDB.movies[a] = b;
+console.log(personaMovieDB);  
